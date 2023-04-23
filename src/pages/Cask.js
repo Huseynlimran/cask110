8c49a77e00b3e722258dbc5447840ea233fb0caf
Imran <huseynlimran@gmail.com>
2023-04-23 09:48:23 +0400

    data

---
+++ src/pages/Cask.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Cask() {
+  return (
+    <div className='container'>
+    </div>
+  )
+}
---
+++ src/pages/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='header'>
+        <img src="img/cassk110.png" alt="" />
+    </div>
+  )
+}
