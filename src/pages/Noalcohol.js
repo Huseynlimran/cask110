a38781c44f72d918a595db4e9cba8b9f6eac9bc5
Imran <89724435+Huseynlimran@users.noreply.github.com>
2023-05-19 19:24:48 +0400

    Update Noalcohol.js

--- src/pages/Noalcohol.js
+++ src/pages/Noalcohol.js
@@ -11,31 +11,9 @@ export default function Noalcohol() {
         <div className="line"></div>
         <p>6 ₼</p>
       </div>
-      <div className="price">
-        <p>Arizona Sunset  </p>
-        <div className="line"></div>
-        <p>6 ₼</p>
-      </div>
-      <div className="price">
-        <p>Rainbow   </p>
-        <div className="line"></div>
-        <p>6 ₼</p>
-      </div>
-      <div className="price">
-        <p>Cinderella   </p>
-        <div className="line"></div>
-        <p>6 ₼</p>
-      </div>
-      <div className="price">
-        <p>Margarita   </p>
-        <div className="line"></div>
-        <p>6 ₼</p>
-      </div>
-      <div className="price">
-        <p>Special   </p>
-        <div className="line"></div>
-        <p>7 ₼</p>
-      </div>
+ 
+    
+   
       <div className="price">
         <p>Limonade   </p>
         <div className="line"></div>
