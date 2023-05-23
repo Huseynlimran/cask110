402d64d39c53f7a7a3332f4519b5e3f096bec66b
Imran <89724435+Huseynlimran@users.noreply.github.com>
2023-05-23 19:12:01 +0400

    Update Chellek.js

--- src/pages/Chellek.js
+++ src/pages/Chellek.js
@@ -7,14 +7,14 @@ export default function Chellek() {
         <h1 className="title">Beer Cask</h1>
       </div>
       <div className="price">
-        <p>Xirdalan 330/500 ml </p>
+        <p>Xirdalan 500 ml </p>
         <div className="line"></div>
-        <p>2.50/3 ₼</p>
+        <p>3 ₼</p>
       </div>
       <div className="price">
-        <p>Xirdalan süzülmemiş 330/500 ml</p>
+        <p>Xirdalan süzülmemiş 500 ml</p>
         <div className="line"></div>
-        <p>3/3.50 ₼</p>
+        <p>3.50 ₼</p>
       </div>
     </div>
   );
